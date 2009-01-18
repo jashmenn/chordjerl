86d536431914d51f5cc709a5652e965147f69a6c
Nate Murray <nate@natemurray.com>
2009-01-17 16:23:53 -0800

    tweaks to function calls

--- test/src/eunit_chordjerl.erl
+++ test/src/eunit_chordjerl.erl
@@ -1,14 +1,13 @@
 -module(eunit_chordjerl).
--export([bar1/0, bar2/0]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 bar1_test_() ->
   [
-    ?_assert(ok == bar1())
+    ?_assert(ok == chordjerl:bar1())
   ].
 
 bar2_test_() ->
   [
-    ?_assert(ok == bar2())
+    ?_assert(ok == chordjerl:bar2())
   ].
