246d10bbc34050941d73ab92fdfecc00dbb06ff4
Nate Murray <nate@natemurray.com>
2009-01-23 19:23:55 -0800

    started utils for working with ids

---
+++ src/ch_id_utils.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% File        : ch_id_utils.erl
+%%% Author      : Nate Murray <nate@natemurray.com>
+%%% Description : Utilities for calculating ids 
+%%% Created     : 2009-01-23
+%%%-------------------------------------------------------------------
+-module(ch_id_utils).
+
+%%--------------------------------------------------------------------
+%% Function: successor_id(CurrentId, Index) -> {ok,SuccessorId} 
+%% Description: given CurrentId and Index returns the minimum Id required by
+%% the I'th successor
+%%--------------------------------------------------------------------
+successor_id(CurrentId, Index) ->
+  {ok}.
