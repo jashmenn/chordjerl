f26164efc4853c17da69ce32a36153d2f17c0faa
Nate Murray <nate@natemurray.com>
2009-01-29 08:36:40 -0800

    trying to simplify to get my head around what the initial fingers should look like

--- test/src/eunit_chordjerl_srv.erl
+++ test/src/eunit_chordjerl_srv.erl
@@ -43,31 +43,40 @@ node_network_functional_test_() ->
          ?NTRACE("state1 is:", [State1]),
 
          % join the second node to the first 
-         Node = gen_server:call(testnode1, {return_node}),
-         ok   = gen_server:call(testnode2, {join, Node}),
+         Node   = gen_server:call(testnode1, {return_node}),
+         ok     = gen_server:call(testnode2, {join, Node}),
+
          State2 = gen_server:call(testnode2, {return_state}),
          ?NTRACE("state2 is:", [State2]),
+
+         % verify fingers
          {srv_state, Fingers, _Predecessor, _Backing, _Sha1} = State2,
          ?assertEqual(1, length(Fingers)),
          Finger1 = lists:last(Fingers),
-         {finger, _Sha2, _Node1} = Finger1,
+         {finger, Sha2, _Node1} = Finger1,
+
+         % successor finger should be... ?
+         % ?assertEqual(State1#srv_state.sha, Sha2),
+         ?assertEqual(State2#srv_state.sha, Sha2),
 
          % here we need to stabilize and make sure the first node becomes
          % connected to the second
          % join the third node to the second
-         Node2  = gen_server:call(testnode2, {return_node}),
-         ok     = gen_server:call(testnode3, {join, Node2}),
-         State3 = gen_server:call(testnode3, {return_state}),
-         ?NTRACE("state3 is:", [State3]),
-         ?assertEqual(1, length(State3#srv_state.fingers)),
+         % Node2  = gen_server:call(testnode2, {return_node}),
+         %ok     = gen_server:call(testnode3, {join, Node2}),
+         %State3 = gen_server:call(testnode3, {return_state}),
+         %?NTRACE("state3 is:", [State3]),
+         %?assertEqual(1, length(State3#srv_state.fingers)),
 
-         {ok, Successor1} = gen_server:call(testnode1, {find_successor, State1#srv_state.sha}),
-         ?NTRACE("testnode1 find successor", [Successor1]),
+         % ---
 
-         %{ok, Successor2} = gen_server:call(testnode2, {find_successor, State2#srv_state.sha}),
-         Response = gen_server:call(testnode2, {find_successor, State2#srv_state.sha}),
+         %{ok, Successor1} = gen_server:call(testnode1, {find_successor, State1#srv_state.sha}),
+         %?NTRACE("testnode1 find successor", [Successor1]),
 
-         ?NTRACE("testnode2 find successor", [Response]),
+         %%{ok, Successor2} = gen_server:call(testnode2, {find_successor, State2#srv_state.sha}),
+
+         %Response = gen_server:call(testnode2, {find_successor, State2#srv_state.sha}),
+         %?NTRACE("testnode2 find successor", [Response]),
 
          %%%  
 
