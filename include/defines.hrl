33ec671c5d7b25b61dd75cb728f46c8020583bbf
Ari Lerner <alerner@YPCMC05591.local>
2009-02-27 13:46:20 -0800

    Added ifdef debug around the NBIT

--- include/defines.hrl
+++ include/defines.hrl
@@ -1,13 +1,17 @@
 % Includes file for the Chordjerl
+-define (DEBUG, true).
 
 -define (DICT, dict).
 -define (TRACE(X, M),  io:format(user, "TRACE ~p:~p ~p ~p~n",           [?MODULE, ?LINE, X, M])).
 -define (NTRACE(X, M), io:format(user, "NTRACE ~p:~p ~p ~p ~p ~p ~p~n", [?MODULE, ?LINE, chordjerl_srv:registered_name(), node(), self(), X, M])).
 -define (RECONNECT_TIMEOUT, 10000).
-%-define (NBIT, 160). % number of bits in the hash function
--define (NBIT, 7). % number of bits in the hash function
+-define (NBIT, 160). % number of bits in the hash function
 -define (MAXFINGERS, ?NBIT). % number of bits in the hash function
 -define (NBITMOD, round(math:pow(2, ?NBIT))).
+
+-ifdef (DEBUG).
+-define (NBIT, 7). % number of bits in the hash function
+-endif.
 
 -define (DEFAULT_CONFIG, []).
 
